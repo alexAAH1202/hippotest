6a8f9c5858be7988784deb825b11e82e348b871e
alexAAH1202 <alex@ampersandalliedhealth.com.au>
2024-10-02 07:02:11 +1000

    Update Home.vue

---
+++ hippo-health-landing/src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <Header />
+    <Hero />
+    <Features />
+    <AboutUs />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header.vue'
+import Hero from '@/components/Hero.vue'
+import Features from '@/components/Features.vue'
+import AboutUs from '@/components/AboutUs.vue'
+import Footer from '@/components/Footer.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Header,
+    Hero,
+    Features,
+    AboutUs,
+    Footer
+  }
+}
+</script>
