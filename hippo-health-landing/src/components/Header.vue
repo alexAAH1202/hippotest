06787d4df63cc09e2515ba77961b000968a74474
alexAAH1202 <alex@ampersandalliedhealth.com.au>
2024-10-02 06:58:24 +1000

    Update Header.vue

---
+++ hippo-health-landing/src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <footer>
+    <div id="contact">
+      <h3>Contact Us</h3>
+      <p>Email: team@hippohealth.care</p>
+      <p>Phone: (123) 456-7890</p>
+    </div>
+    <p>&copy; 2023 Hippo Health. All rights reserved.</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+
+<style scoped>
+/* Add your styles here */
+</style>
